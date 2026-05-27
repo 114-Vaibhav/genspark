71cc8e9dfe1922a648848d5bc5012d4fa6d45922
Vaibhav Gupta <vg9400313@gmail.com>
2026-05-27 09:35:13 +0530

    day 23 may 26

---
+++ Day 23 May 26/BankingAPI/Interfaces/ICustomerInteract.cs
@@ -0,0 +1,23 @@
+﻿using BankingAPI.Models;
+using BankingAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankingAPI.Interfaces
+{
+    public interface ICustomerInteract
+    {
+        public Task<CreateAccountResponse> OpensAccount(CreateAccountRequest account);
+        public Task<GetAccountResponse> GetAccountByAccountNumber(string accountNumber);
+
+
+        // public CreateAccountResponse OpensAccount(CreateAccountRequest account);
+        // public GetAccountResponse GetAccountByAccountNumber(string accountNumber);
+       
+
+    }
+}
